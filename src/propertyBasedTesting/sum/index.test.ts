96b05eef512395c7551d5f4804cc0492668c74c6
Nha-Na-mo7 <zirconiump42@gmail.com>
2025-01-22 20:18:38 +0900

    feat(pbt): add sum associate test

--- src/propertyBasedTesting/sum/index.test.ts
+++ src/propertyBasedTesting/sum/index.test.ts
@@ -5,15 +5,27 @@ import { fc, it } from "@fast-check/jest";
  * fc.integer(): 値のジェネレータ。指定の型の値を生成し、該当の引数に渡す。
  */
 
-/**
- * @remarks
- * 交換法則のテスト
- * 引数を入れ替えても結果が同一であるという性質
- */
-it("[PBT]sum", () => {
-  fc.assert(
-    fc.property(fc.integer(), fc.integer(), (num1, num2) => {
-      return sum(num1, num2) === sum(num2, num1);
-    })
-  );
+describe("[PBT]sum", () => {
+  /**
+   * @remarks
+   * 交換法則のテスト
+   */
+  it("引数が入れ替わっても結果が同一となること", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (num1, num2) => {
+        return sum(num1, num2) === sum(num2, num1);
+      })
+    );
+  });
+  /**
+   * @remarks
+   * 結合法則のテスト
+   */
+  it("結合箇所が変化しても結果が同一となること", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), fc.integer(), (x, y, z) => {
+        return sum(x, sum(y, z)) === sum(sum(x, y), z);
+      })
+    );
+  });
 });
