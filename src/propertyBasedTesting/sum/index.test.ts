52d021ea31a60b4daace31695fda2fecafc5b316
Nha-Na-mo7 <zirconiump42@gmail.com>
2025-01-22 21:58:52 +0900

    feat(pbt): add first shrunk

--- src/propertyBasedTesting/sum/index.test.ts
+++ src/propertyBasedTesting/sum/index.test.ts
@@ -1,4 +1,4 @@
-import { sum } from "./index";
+import { notSum, sum } from "./index";
 import { fc, it } from "@fast-check/jest";
 
 /**
@@ -25,6 +25,29 @@ describe("[PBT]sum", () => {
     fc.assert(
       fc.property(fc.integer(), fc.integer(), fc.integer(), (x, y, z) => {
         return sum(x, sum(y, z)) === sum(sum(x, y), z);
+      })
+    );
+  });
+  // 性質は間違っているがテストが通る例
+  it("性質は間違っているがテストは通ってしまう例", () => {
+    fc.assert(
+      fc.property(fc.integer(), fc.integer(), (num1, num2) => {
+        return notSum(num1, num2) === notSum(num2, num1);
+      })
+    );
+  });
+  // テストが成立しない例
+  /**
+   * Property failed after 1 tests
+   * { seed: -6926597, path: "0:1:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0", endOnFailure: true }
+   * Counterexample: [1]
+   * Shrunk 31 time(s) // 反例が出現する最小の値
+   * Got error: Property failed by returning false
+   */
+  it.skip("テストが成立しない例", () => {
+    fc.assert(
+      fc.property(fc.integer(), (num1) => {
+        return notSum(num1, 0) === num1;
       })
     );
   });
--- src/propertyBasedTesting/sum/index.ts
+++ src/propertyBasedTesting/sum/index.ts
@@ -1,3 +1,4 @@
 type Sum = (num1: number, num2: number) => number;
 
 export const sum: Sum = (num1, num2) => num1 + num2;
+export const notSum: Sum = (num1, num2) => 0;
