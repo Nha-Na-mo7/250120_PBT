7f52b8e1ebe0e2cec1821b4ba3440c93bae46cae
Nha-Na-mo7 <zirconiump42@gmail.com>
2025-01-22 12:34:29 +0900

    feat(pbt): add comment commutative law

--- src/propertyBasedTesting/sum/index.test.ts
+++ src/propertyBasedTesting/sum/index.test.ts
@@ -4,6 +4,12 @@ import { fc, it } from "@fast-check/jest";
 /**
  * fc.integer(): 値のジェネレータ。指定の型の値を生成し、該当の引数に渡す。
  */
+
+/**
+ * @remarks
+ * 交換法則のテスト
+ * 引数を入れ替えても結果が同一であるという性質
+ */
 it("[PBT]sum", () => {
   fc.assert(
     fc.property(fc.integer(), fc.integer(), (num1, num2) => {
